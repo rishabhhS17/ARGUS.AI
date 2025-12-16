52f517ddf9cc714ab96d6638f4ccd03be3ea9467
Rishabh Singh <irishabhsingh17@gmail.com>
2025-12-16 19:39:16 +0530

    Fixed unused component

--- client/src/components/LandingPage.tsx
+++ client/src/components/LandingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import { useRef } from "react";
 import { Link } from "react-router-dom";
 import { motion, useScroll, useTransform } from "framer-motion";
 import { 
